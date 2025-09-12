b79db12097ae6be2df4e21b9d40767f7f643b655
AbdElrhman Mohamed <abdelrhman132006@gmail.com>
2025-09-12 21:32:33 +0300

    Add files via upload

---
+++ Day 6 C#2/TimeDataType/TimeDataType/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace program
+{
+    class Time
+    {
+        // private fields:
+        private int Hours;
+        private int Minutes;
+        private int Seconds;
+
+        // public constructor:
+        public Time(int hours, int minutes, int seconds)
+        {
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;
+        }
+
+        // Getter method:
+        public string Get()
+        {
+            return $"{Hours}H:{Minutes}M:{Seconds}S";
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create new time object:
+            Time time = new Time(22, 33, 11);
+            string info = time.Get();
+            Console.WriteLine(info);
+        }
+    }
+}
