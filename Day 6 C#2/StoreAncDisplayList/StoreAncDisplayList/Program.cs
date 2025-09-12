8d23977ace9b8b7f525249272b51d2089235024f
AbdElrhman Mohamed <abdelrhman132006@gmail.com>
2025-09-12 14:23:20 +0300

    Add files via upload

---
+++ Day 6 C#2/StoreAncDisplayList/StoreAncDisplayList/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace project
+{
+    internal class Project
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of students: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            string[] students = new string[num];
+
+            // First loop: fill array with names
+            for (int i = 0; i < num; i++)
+            {
+                Console.Write($"Enter student {i + 1} name: ");
+                students[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("\n--- Student List ---");
+
+            // Second loop: print names (using foreach)
+            foreach (string student in students)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+}
