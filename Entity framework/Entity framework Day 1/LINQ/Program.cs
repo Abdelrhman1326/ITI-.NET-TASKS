91cc8b7c2d5514895d28641a2ff2cf7aa8a73a73
Abdelrhman132006 <abdelrhman132006@gmail.com>
2025-09-23 13:20:37 +0300

    Third Query implemented

--- Entity framework/Entity framework Day 1/LINQ/Program.cs
+++ Entity framework/Entity framework Day 1/LINQ/Program.cs
@@ -1,17 +1,23 @@
-﻿namespace project
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace project
 {
     internal class Project
     {
         public static void Main(string[] args)
         {
+            #region 
             // list
             List<int> numbers = new List<int>() { 2, 4, 6, 7, 1, 4, 2, 9, 1 };
 
+
             // Query 1: Display numbers without any repeated Data and sorted
             var uniqueSortedNumbers = numbers.Distinct().OrderBy(n => n);
-            // test query 1:
-            string result = string.Join(", ", uniqueSortedNumbers);
-            Console.WriteLine(result + '\n');
+            string result0 = string.Join(", ", uniqueSortedNumbers);
+            Console.WriteLine(result0 + '\n');
+
 
             // Query2: using Query1 result and show each number and it’s multiplication
             Dictionary<int, long> PowOfTwo = new Dictionary<int, long>();
@@ -21,8 +27,21 @@
             }
             foreach (var pair in PowOfTwo)
             {
-                Console.WriteLine($"( Number = {pair.Key}, Multiply = {pair.Value} )");
+                Console.WriteLine($"( Number = {pair.Key}, Multiply = {pair.Value} )" + '\n');
             }
+            #endregion
+
+            #region 
+            // array:
+            string[] names = ["Tom", "Dick", "Harry", "MARY", "Jay"];
+
+
+            // Query1: Select names with length equal 3.
+            var namesOfLengthThree = names.Where(name => name.Length == 3);
+            string result1 = string.Join(", ", namesOfLengthThree);
+            Console.WriteLine(result1 + '\n');
+
+            #endregion
         }
     }
 }
